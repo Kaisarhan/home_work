7225cd9c98015111665437772839153c79b7d3df
Kaisarhan <kaisarhan@mail.ru>
2025-08-29 22:35:22 +0500

    end all home work

---
+++ nginx-manager.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CONTAINER_NAME="my-nginx"
+
+case $1 in
+  start)
+    echo "Запуск контейнера..."
+    docker start $CONTAINER_NAME
+    ;;
+  stop)
+    echo "Остановка контейнера..."
+    docker stop $CONTAINER_NAME
+    ;;
+  restart)
+    echo "Перезапуск контейнера..."
+    docker restart $CONTAINER_NAME
+    ;;
+  status)
+    echo "Статус контейнера:"
+    docker ps -a --filter name=$CONTAINER_NAME
+    ;;
+  logs)
+    echo "Логи контейнера:"
+    docker logs $CONTAINER_NAME
+    ;;
+  cleanup)
+    echo "Очистка неиспользуемых ресурсов Docker..."
+    docker image prune
+    ;;
+  *)
+    echo "Использование: $0 {start|stop|restart|status|logs}"
+    exit 1
+    ;;
+esac
